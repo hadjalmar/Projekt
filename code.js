e79bf0ae33e46a15102e968d6d7a8f8504b7cb1b
Baluka2006 <k.balazs.2006@gmail.com>
2025-10-09 10:55:23 +0200

    JSON file feldolgozása

--- code.js
+++ code.js
@@ -3,3 +3,13 @@ let cards = [];
 let firstCard, secondCard;
 let lockBoard = false;
 let score = 0;
+
+document.querySelector(".score").textContent = score;
+
+fetch("./memory.json")
+    .then((res) => res.json())
+    .then((data) => {
+        cards = [...data, ...data];
+        shuffleCards();
+        generateCards();
+    });
