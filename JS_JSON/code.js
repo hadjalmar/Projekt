293bae065af209753c3975361bb5adfb325d2150
Baluka2006 <k.balazs.2006@gmail.com>
2025-10-13 20:17:16 +0200

    Párosítós játék alapok

--- JS_JSON/code.js
+++ JS_JSON/code.js
@@ -158,3 +158,40 @@ function restart() {
         generateCards();
     }, 600);
 }
+
+
+
+
+
+
+let dragItem = document.getElementById('dragItem');
+let divs = document.querySelectorAll('div')
+console.log(divs)
+let div = document.querySelector('div')
+console.log(div)
+
+divs.forEach(dropZone => {
+  dropZone.addEventListener('dragover', (e) => {
+    console.log(e);
+    e.preventDefault();
+    dropZone.appendChild(dragItem);
+  });
+
+  dropZone.addEventListener('dragover', () => {
+    dropZone.classList.add('hoverOver');
+  });
+
+  dropZone.addEventListener('dragleave', () => {
+    dropZone.classList.remove('hoverOver');
+  });
+
+});
+
+dragItem.addEventListener('drag', () => {
+  dragItem.classList.add('beingDragged');
+})
+
+
+dragItem.addEventListener('dragend', () => {
+  dragItem.classList.remove('beingDragged');
+})
