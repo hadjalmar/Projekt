8200d014a62c00417ea3e667f12670e28c817800
Baluka2006 <k.balazs.2006@gmail.com>
2025-10-14 08:47:49 +0200

    drag and drop javítása

--- JS_JSON/matchCode.js
+++ JS_JSON/matchCode.js
@@ -24,16 +24,54 @@ let kepek = [
 ];
 
 function Feltolt() {
-    let dragSzekcio = document.querySelector(".dragZone");
-    let dropSzekcio = document.querySelector(".dropZone");
+  let dragSzekcio = document.querySelector(".dragZoneSzavak");
+  let dropSzekcio = document.querySelector(".dragZoneKepek");
 
-    szakszavak.forEach(szo => {
-        dragSzekcio.innerHTML += `<p class="dragElement" draggable="true">${szo}</p>`;
-    });
+  szakszavak.forEach(szo => {
+    dragSzekcio.innerHTML += `<p class="dragElement" draggable="true">${szo}</p>`;
+  });
 
-    for (let i = 0; i < kepek.length; i++) {
-        dropSzekcio.innerHTML += `<img src="../parositoKepek/${kepek[i]}" alt="${szakszavak[i]}">`;
-    }
+  for (let i = 0; i < kepek.length; i++) {
+    dropSzekcio.innerHTML += 
+        `
+          <div class="elemek">
+            <img src="../parositoKepek/${kepek[i]}" alt="${szakszavak[i]}">
+            <div class="dropZone"></div>
+          </div>
+        `;
+  }
 };
+
+let dragItem = document.querySelector('.elemek');
+let divs = document.querySelectorAll('.dropZone');
+console.log(divs)
+let div = document.querySelector('div')
+console.log(div)
+
+divs.forEach(dropZone => {
+  dropZone.addEventListener('dragover', (e) => {
+    console.log(e);
+    e.preventDefault();
+    dropZone.appendChild(dragItem);
+  });
+
+  dropZone.addEventListener('dragover', () => {
+    dropZone.classList.add('hoverOver');
+  });
+
+  dropZone.addEventListener('dragleave', () => {
+    dropZone.classList.remove('hoverOver');
+  });
+
+});
+
+dragItem.addEventListener('drag', () => {
+  dragItem.classList.add('beingDragged');
+})
+
+
+dragItem.addEventListener('dragend', () => {
+  dragItem.classList.remove('beingDragged');
+})
 
 Feltolt();
